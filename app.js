c74633fca822f8a354ad39cb208fa0bb2f5f49b9
rameskris <redmiblack2@gmail.com>
2022-03-16 08:15:41 +0530

    second commit

--- app.js
+++ app.js
@@ -8,14 +8,41 @@ bigOofn(10)
 
 //Drop constants
 //1st rule is drop constents
-function logItems(n){
-    for(let i=0;i<n;i++){
-        console.log(i)           //o(n)+o(n)=o(2n)=>drop the constant here its 2 so O(n)
 
-    }
-    for(let j=0;j<n;j++){
-        console.log(j)
+// function logItems(n){
+//     for(let i=0;i<n;i++){
+//         console.log(i)           //o(n)+o(n)=o(2n)=>drop the constant here its 2 so O(n)
+
+//     }
+//     for(let j=0;j<n;j++){
+//         console.log(j)
+//     }
+// }
+
+//logItems(3)
+
+//o(n2)
+function logItemsOofnsq(n){
+    for(let i=0;i<n;i++){
+        for(let j=0;j<n;j++){       //o(n*n)=>o(n^2) 
+           
+                console.log(i,j)
+                
+            
+        }
     }
 }
 
-logItems(3)
+function logItemsOofntrip(n){
+    for(let i=0;i<n;i++){
+        for(let j=0;j<n;j++){  
+            for(let j=0;j<n;j++){      //o(n*n*n)=>o(n^3) it doesnt matter o(n^3) its just o(n^2)
+           
+                console.log(i,j)
+            }  
+            
+        }
+    }
+}
+
+logItemsOofnsq(3)
